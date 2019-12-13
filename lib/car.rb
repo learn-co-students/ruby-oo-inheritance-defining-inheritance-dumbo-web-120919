92bc9279d086fe76c5e5208623f23a45e6044eee
Jessica Natovich <jessicanatovich@yahoo.com>
2019-12-13 14:16:41 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,13 @@
-require_relative "./vehicle.rb"
+describe "#go" do
+    it "returns 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'" do 
+    it "returns 'vrrrrrrrooom!'" do 
+      expect(car.go).to eq("VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!")
+    end 
+  end
+@@ -34,4 +34,4 @@
+      expect(car.fill_up_tank).to eq("filling up!")
+    end
+  end
+end
+end 
+0 comments on commit 0d36865
